008ed48ea1f447c879b12e61270244fe78b97d7c
James Fraser <fwaggle@fwaggle.org>
2017-11-27 12:18:09 +1100

    License updates.

--- elm327/elm327.py
+++ elm327/elm327.py
@@ -1,6 +1,6 @@
 """
 Python module presenting an API to an ELM327 serial interface
-(C) 2015 Jamie Fraser <fwaggle@fwaggle.org>
+(C) 2015-2017 James Fraser <fwaggle@fwaggle.org>
 http://github.com/fwaggle/pyELM327
 
 Please see License.txt and Readme.md.
